d3a5edf816adcd11ff5178a6b2ef79f98731cec2
Dmytro <51440817+Bezushchak@users.noreply.github.com>
2021-08-06 13:38:09 +0300

    Add files via upload

---
+++ custom_ner_training.py
@@ -0,0 +1,44 @@
+from typing import List
+from flair.datasets import ColumnCorpus
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+from flair.data import Corpus
+from flair.embeddings import WordEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from typing import List
+
+from flair.embeddings import *
+
+columns = {0 : 'text', 1 : 'ner'}
+data_folder = '/Users/Dmytro_Bezushchak/Desktop/NLP_app'
+
+# Initializing the corpus
+corpus: Corpus = ColumnCorpus(data_folder, columns,
+                              train_file = 'train.txt',
+                              test_file = 'test.txt',
+                              dev_file = 'dev.txt')
+print(len(corpus.train))
+print(corpus.train[0].to_tagged_string('ner'))
+
+tag_type = 'ner'
+tag_dictionary1 = corpus.make_tag_dictionary(tag_type=tag_type)
+
+embedding_types: List[TokenEmbeddings] = [
+        WordEmbeddings('glove'),
+        # other embeddings
+        ]
+embeddings: StackedEmbeddings = StackedEmbeddings(
+                                embeddings=embedding_types)
+
+tagger : SequenceTagger = SequenceTagger(hidden_size=256,
+                                        embeddings=embeddings,
+                                        tag_dictionary=tag_dictionary1,
+                                        tag_type=tag_type,
+                                        use_crf=True)
+                                        
+trainer : ModelTrainer = ModelTrainer(tagger, corpus)
+trainer.train('resources/taggers/example-ner',
+               learning_rate=0.1,
+               mini_batch_size=32,
+               max_epochs=150)
+print(tagger)
